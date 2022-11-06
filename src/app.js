10184386063f6395e6940196a9450eac658d4d9e
Jeehay Park <sophiepark528@gmail.com>
2022-11-07 01:32:48 +0900

    Fix open share menu image

--- src/app.js
+++ src/app.js
@@ -453,7 +453,7 @@ export class App extends LitElement {
 	 */
 	async showHelpToast() {
 		const { showSnackbar } = await import("./util/show-snackbar.js");
-		showSnackbar(`Web Skills is an overview of useful skills to learn as a web developer`, {
+		showSnackbar(`A visual overview of RPA using web-skills`, {
 			timeout: 1000 * 20,
 			wide: true,
 			buttons: [
